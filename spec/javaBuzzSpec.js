a24b8d99e3e1be5a5aa7fe067c897261254e6186
charlierdm <charlie.davismiller@gmail.com>
2021-03-08 17:22:22 +0000

    added says method and refactored

---
+++ spec/javaBuzzSpec.js
@@ -0,0 +1,50 @@
+describe('Javabuzz', function() {
+  let javabuzz;
+
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
+  describe('knows when a number is', function(){
+    it('is divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true)
+    });
+
+    it('is divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false)
+    });
+
+    it('is divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true)
+    });
+
+    it('is divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(4)).toBe(false)
+    });
+
+    it('is divisible by 3 and 5', function(){
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true)
+    });
+
+    it('is divisible by 3 and 5', function(){
+      expect(javabuzz.isDivisibleByFifteen(10)).toBe(false)
+    });
+
+  });
+
+  describe('when playing, says', function(){
+    it('"Java", when a number is divisible by 3', function(){
+      expect(javabuzz.says(3)).toEqual('Java');
+    });
+
+    it('"Buzz", when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual('Buzz')
+    });
+
+    it('"JavaBuzz", when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual('JavaBuzz')
+    });
+
+  });
+
+});
